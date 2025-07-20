11b734a19d8f9ba34b42221eb8d1fac8639ebbae
satyasootar <satya.sootar06@gmail.com>
2025-07-20 09:40:50 +0530

    fix: Correct image height classes for consistent styling in Gallery component

--- src/components/Gallery.jsx
+++ src/components/Gallery.jsx
@@ -7,8 +7,8 @@ export const Gallery = () => {
                 position: "relative",
                 width: "100%",
                 overflow: "hidden",
-maskImage: "linear-gradient(to right, transparent 0%, black 5%, black 95%, transparent 100%)",
-WebkitMaskImage: "linear-gradient(to right, transparent 0%, black 5%, black 95%, transparent 100%)",
+                maskImage: "linear-gradient(to right, transparent 0%, black 5%, black 95%, transparent 100%)",
+                WebkitMaskImage: "linear-gradient(to right, transparent 0%, black 5%, black 95%, transparent 100%)",
 
 
                 display: "flex",
@@ -16,14 +16,16 @@ WebkitMaskImage: "linear-gradient(to right, transparent 0%, black 5%, black 95%,
             }}>
 
                 <div className="flex flex-cols gap-6 items-end ">
-                    <img className="border-2 rounded-sm hover:border-sky-300 laptop:max-h-[25rem] mobile:max-h-[15rem]" src="./Gallery/boat.jpg" />
-                    <img className="border-2 rounded-sm hover:border-sky-300 laptop:max-h-[25rem] mobile:max-h-[15rem]" src="./Gallery/Boys.jpg" />
-                    <img className="border-2 rounded-sm hover:border-sky-300 laptop:max-h-[25rem] mobile:max-h-[15rem]" src="./Gallery/plane.jpg" />
-                    <img className="border-2 rounded-sm hover:border-sky-300 laptop:max-h-[25rem] mobile:max-h-[15rem]" src="./Gallery/smile.jpg" />
-                    <img className="border-2 rounded-sm hover:border-sky-300 laptop:max-h-[25rem] mobile:max-h-[15rem]" src="./Gallery/sunset.jpg" />
-                    <img className="border-2 rounded-sm hover:border-sky-300 laptop:max-h-[25rem] mobile:max-h-[15rem]" src="./Gallery/Synbot.jpeg" />
-                    <img className="border-2 rounded-sm hover:border-sky-300 laptop:max-h-[25rem] mobile:max-h-[15rem]" src="./Gallery/Webdesign.jpg" />
-                    <img className="border-2 rounded-sm hover:border-sky-300 laptop:max-h-[25rem] mobile:max-h-[15rem]" src="./Gallery/Waterfall.jpg" />
+                    <img className="border-2 rounded-sm hover:border-sky-300 laptop:max-h-[25rem] max-h-[15rem]" src="./Gallery/boat.jpg" />
+                    <img className="border-2 rounded-sm hover:border-sky-300 laptop:max-h-[25rem] max-h-[15rem]" src="./Gallery/boat.jpg" />
+                    <img className="border-2 rounded-sm hover:border-sky-300 laptop:max-h-[25rem] max-h-[15rem]" src="./Gallery/boat.jpg" />
+                    <img className="border-2 rounded-sm hover:border-sky-300 laptop:max-h-[25rem] max-h-[15rem]" src="./Gallery/Boys.jpg" />
+                    <img className="border-2 rounded-sm hover:border-sky-300 laptop:max-h-[25rem] max-h-[15rem]" src="./Gallery/plane.jpg" />
+                    <img className="border-2 rounded-sm hover:border-sky-300 laptop:max-h-[25rem] max-h-[15rem]" src="./Gallery/smile.jpg" />
+                    <img className="border-2 rounded-sm hover:border-sky-300 laptop:max-h-[25rem] max-h-[15rem]" src="./Gallery/sunset.jpg" />
+                    <img className="border-2 rounded-sm hover:border-sky-300 laptop:max-h-[25rem] max-h-[15rem]" src="./Gallery/Synbot.jpeg" />
+                    <img className="border-2 rounded-sm hover:border-sky-300 laptop:max-h-[25rem] max-h-[15rem]" src="./Gallery/Webdesign.jpg" />
+                    <img className="border-2 rounded-sm hover:border-sky-300 laptop:max-h-[25rem] max-h-[15rem]" src="./Gallery/Waterfall.jpg" />
                 </div>
 
             </Marquee>
